71cf826916e229ef2ad1eda719f26591e1841356
Williams Hernández <hernandw@gmail.com>
2023-08-28 10:11:30 -0400

    cambio de url en frontend

--- client/src/pages/Login.jsx
+++ client/src/pages/Login.jsx
@@ -11,7 +11,7 @@ const Login = () => {
     e.preventDefault();
     try {
       const response = await axios.post(
-        "http://localhost:3000/login",
+        "https://plantasbk.onrender.com/login",
         {
           email,
           password,
